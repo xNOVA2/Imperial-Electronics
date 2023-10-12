e158d32c40a2682207866ccbe9738c9f54a0ac54
ali usaid <aliusaid55@gmail.com>
2023-10-12 05:06:18 -0700

    Fix Order Page

--- app/AdminOrderPageSystem/page.tsx
+++ app/AdminOrderPageSystem/page.tsx
@@ -1,63 +1,55 @@
 'use client'
-import { Button } from "@/components/ui/button";
 import axios from "axios";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-export default async function OrderPage() {
-    const router = useRouter()
-    useEffect(() => {
-        router.refresh()
-    
-     
-    }, [])
-    
-    const data = await axios.get(`api/GetOrders`,);
+import { useRouter } from "next/navigation"; // Change 'next/navigation' to 'next/router'
+import { useEffect, useState } from "react"; // Import 'useState' for managing state
+
+export default function OrderPage() {
+    const router = useRouter();
+  const [orders, setOrders] = useState([]); // State to hold fetched orders
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("/api/GetOrders");
+      setOrders(response.data.data); // Update the state with fetched data
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
   return (
     <div>
-    <nav className="flex justify-between px-4 items-center">
-      <h1 className="text-2xl font-bold">
-        <Link href={'/AdminPanel/AyanDashboard'}>Admin Dashboard</Link>
-      </h1>
-      <ul className="flex justify-items-center m-3 gap-4">
-        <li>
-          <Button variant={"link"} className="text-sm">
-            <Link href={'/AdminPanel/AyanDashboard/Update&Delete'} > Update / Delete Products</Link>
-          </Button>
-        </li>
-     
-        <li><Button variant={"link"}><Link href={'/AdminPanel/AyanDashboard/AddProduct'}> Add new Product</Link></Button></li>
-        
-      </ul>
-    </nav>
-
-    {/* Order Code */}
-    <div className="flex justify-center">
-      <h1 className="text-4xl font-bold">Orders</h1>
-    </div>
-    {data.data.data.slice().reverse().map((order:any, index:number) => (
-      <div key={index} className="p-5">
-        <p>{`Order by ${order.Name}`}</p>
-        <br />
-        <p>{`Address: ${order.Address}`}</p>
-        <br />
-        <p>{`Phone Number: ${order.PhoneNumber}`}</p>
-
-        {/* Display items purchased */}
-        <div>
-          <h2 className="text-2xl font-semibold mt-4">Items Purchased:</h2>
-          <ul>
-            {order.orderItems.map((item:any, itemIndex:number) => (
-              <li key={itemIndex}>{`${item.modelNumber} by ${item.company}`}</li>
-            ))}
-          </ul>
-        </div>
-
-        <hr />
+      {/* Your navigation and header code here */}
+      
+      {/* Order Code */}
+      <div className="flex justify-center">
+        <h1 className="text-4xl font-bold">Orders</h1>
       </div>
-    ))}
-  </div>
-
-  )
+      
+      {orders.map((order:any, index:number) => (
+        <div key={index} className="p-5">
+          <p>{`Order by ${order.Name}`}</p>
+          <br />
+          <p>{`Address: ${order.Address}`}</p>
+          <br />
+          <p>{`Phone Number: ${order.PhoneNumber}`}</p>
+  
+          {/* Display items purchased */}
+          <div>
+            <h2 className="text-2xl font-semibold mt-4">Items Purchased:</h2>
+            <ul>
+              {order.orderItems.map((item:any, itemIndex:number) => (
+                <li key={itemIndex}>{`${item.modelNumber} by ${item.company}`}</li>
+              ))}
+            </ul>
+          </div>
+  
+          <hr />
+        </div>
+      ))}
+    </div>
+  );
 }
